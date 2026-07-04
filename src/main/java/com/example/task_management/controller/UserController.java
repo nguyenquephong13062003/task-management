6350f51bff3a6882181eab5e68d4e34237ba3af2
Nguyen Que Phong <nguyenquephong13062003@gmail.com>
2026-07-04 16:14:55 +0700

    feat: create controller UserController and TaskController [exercise02]

---
+++ src/main/java/com/example/task_management/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.task_management.controller;
+
+import com.example.task_management.model.User;
+import com.example.task_management.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+}
